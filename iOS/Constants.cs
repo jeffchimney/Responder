748aa422d66cd5b42edcc7c5bbba37ab7039f9c5
Jeff Chimney <jeffchimney@Jeffs-iMac.local>
2018-03-14 18:56:23 -0700

    constants with placeholders

--- iOS/Constants.cs
+++ iOS/Constants.cs
@@ -3,6 +3,10 @@ namespace Responder.iOS
 {
     public class Constants
     {
+        // Azure app-specific connection string and hub path
+        public const string ConnectionString = "<Azure connection string>";
+        public const string NotificationHubPath = "<Azure hub path>";
+
         public Constants()
         {
         }
